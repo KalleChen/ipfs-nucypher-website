218a30c0d82d2d092eb91bf198603b90df11414c
Kalle Chen <kallechen.tw@gmail.com>
2022-04-29 14:50:32 +0800

    Add download function

--- app.py
+++ app.py
@@ -15,14 +15,33 @@ def index():
             file = request.files["upload_file"]
             filename = file.filename
             if file and filename:
+                print(file)
                 file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
                 file.save(file_path)
-                ipfs_hash = upload_ipfs(file_path)
+                ipfs_hash = upload_ipfs(file_path, filename)
+                os.remove(file_path)
                 return render_template("index.html", ipfs_hash=ipfs_hash)
     except Exception as e:
         print(e)
         return render_template("index.html", error=str(e))
     return render_template("index.html")
+
+
+@app.route("/download", methods=["GET"])
+def download():
+    try:
+        if "file_hash" in request.values:
+            ipfs_hash = request.values["file_hash"]
+            if not ipfs_hash:
+                return render_template("download.html")
+            return render_template(
+                "download.html",
+                file_url=f"http://0.0.0.0:8080/ipfs/{ipfs_hash}",
+            )
+        return render_template("download.html")
+    except Exception as e:
+        print(e)
+        return render_template("download.html", error=str(e))
 
 
 if __name__ == "__main__":
--- ipfs.py
+++ ipfs.py
@@ -1,8 +1,14 @@
+import os
 import ipfsApi
 
 
-def upload_ipfs(file_path):
-    api = ipfsApi.Client("http://ipfs", 5001)
-    res = api.add(file_path)
-    print(res)
-    return res
+def upload_ipfs(file_path, file_name):
+    with open(file_path, "rb") as f:
+        file = f.read()
+        with open(file_name, "wb") as f:
+            f.write(file)
+            api = ipfsApi.Client("http://ipfs", 5001)
+            res = api.add(file_name)
+            print(res)
+            os.remove(file_name)
+            return res
